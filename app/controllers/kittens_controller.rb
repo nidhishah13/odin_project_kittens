13385599358670df875a50c5eebe0fd6aed81453
Nidhi <nidhishah1311@gmail.com>
2018-03-13 12:25:23 +0530

    Json API done

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,10 +2,21 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @kittens }
+      format.json { render :json => @kittens }
+    end
   end
 
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @kitten }
+    end
   end
 
   def new 
